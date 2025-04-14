36040b68e6ad03ea97da101b12cf94d1530fcbd3
ratchet34 <maxencefiorina@hotmail.fr>
2025-04-15 01:22:32 +0200

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,10 +19,11 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
+  console.log("Received background message ", payload);
+  const notificationTitle = payload.notification?.title;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
+    body: payload.notification?.body,
+    icon: payload.notification?.image,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
