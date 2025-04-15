a19a6e65a9e5da6a11e4f8ec5bebc595d83ec56a
ratchet34 <maxencefiorina@hotmail.fr>
2025-04-15 14:55:59 +0200

    new try

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import reactNativeWeb from "vite-plugin-react-native-web";
+import { readFileSync } from "fs";
+import * as esbuild from "esbuild";
+
+const extensions = [
+  ".mjs",
+  ".web.tsx",
+  ".tsx",
+  ".web.ts",
+  ".ts",
+  ".web.jsx",
+  ".jsx",
+  ".web.js",
+  ".js",
+  ".css",
+  ".json",
+];
+
+const rollupPlugin = (matchers) => ({
+  name: "js-in-jsx",
+  load(id) {
+    if (matchers.some((matcher) => matcher.test(id)) && id.endsWith(".js")) {
+      const file = readFileSync(id, { encoding: "utf-8" });
+      return esbuild.transformSync(file, { loader: "jsx", jsx: "automatic" });
+    }
+  },
+});
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,6 +42,16 @@ export default defineConfig({
     },
   },
   build: {
+    rollupOptions: {
+      plugins: [rollupPlugin([/react-native-vector-icons/])],
+    },
     sourcemap: true, // Enable source maps in the build
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      jsx: "automatic",
+      loader: { ".js": "jsx" },
+    },
   },
 });
